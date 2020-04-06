3c53d594738706b1db9823d5561275aa7cb754d0
mitsugu3 <mitsugu.pp@gmail.com>
2020-04-06 21:16:55 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,9 @@ class ApplicationController < ActionController::Base
   
 
   def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '2222'
+    http_basic_authenticate_with :name => "admin", :password => "2222", :except => :index
+
+
     end
   end
 end
