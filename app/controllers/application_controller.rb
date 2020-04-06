c80357801a0b2ed2ec34f9c2c81213eca887d6e1
mitsugu3 <mitsugu.pp@gmail.com>
2020-04-06 20:26:39 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'freemarket' && password == 'freemarket66b'
+      username == 'frem' && password == '1234'
     end
   end
 end
