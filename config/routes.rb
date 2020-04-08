a9187a174006e970963bb50da12391ef97239bcd
mitsugu3 <mitsugu.pp@gmail.com>
2020-04-08 10:32:44 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-
-
-
-
   get 'product/show'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
