d254b2c91d5c95f04bc45408c38c56480a5bb807
renkonzero <58683393+renkonzero@users.noreply.github.com>
2020-04-12 22:01:28 +0900

    Delete routes.rb

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-  root 'users#index'
-  get 'users/index'
-  get 'users/menu' => "users#menu"
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
