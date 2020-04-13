5040a627c12962edf9866cb8138d38e0311558f5
renkonzero <58683393+renkonzero@users.noreply.github.com>
2020-04-14 02:10:29 +0900

    指摘箇所を修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'users#index'
-  get 'users/index'
-  get 'users/menu' => "users#signupmean"
-
-
+  get 'messages/index'
+  get 'product/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "messages#index"
+  
 end
