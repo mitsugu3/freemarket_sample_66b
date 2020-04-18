f1269bdcef042926ff4b757e403c575d3c5c39b1
Mariko Umeki <plasticdesign005@gmail.com>
2020-04-18 13:10:14 +0900

    indexにページを表示するようにしました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,9 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth
 
   
-
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == 'admin' && password == '2222'
     end
   end
 end
