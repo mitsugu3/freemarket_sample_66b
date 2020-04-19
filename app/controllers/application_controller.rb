e6c1ba14aaa5c36eba06aa2323aa095a0a6564a5
Mariko Umeki <plasticdesign005@gmail.com>
2020-04-20 00:56:27 +0900

    ヘッダーを部分テンプレート化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
-  before_action :basic_auth
-
-  
-
-   def basic_auth
-     authenticate_or_request_with_http_basic do |username, password|
-       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-     end
-   end
 end
