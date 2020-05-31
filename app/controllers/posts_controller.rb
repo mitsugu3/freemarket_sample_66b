2a4f903e2d2d94f0949fffbabad3149accc7677b
mitsugu3 <mitsugu.pp@gmail.com>
2020-05-31 10:51:21 +0900

    修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,5 @@
 class PostsController < ApplicationController
   def index
     @product = Product.all.order('created_at DESC').limit(3)
-    # リミットの指定
-    # order('created_at DESC').limit(3)
   end
 end
