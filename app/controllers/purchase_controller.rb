343fd4035f5fa1b8943e9608bfa1a49a6e6fff4f
mitsugu3 <mitsugu.pp@gmail.com>
2020-06-06 20:58:34 +0900

    修正

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -1,6 +1,6 @@
 class PurchaseController < ApplicationController
+  before_action :set_card,only: [:index, :pay]
   before_action :set_product
-  before_action :set_card
   require 'payjp'
 
   def index
